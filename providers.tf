f1ea55b6fc6244a0c6e6adbfb399b8596c7f88cf
gmezan <meza.gustavo@pucp.edu.pe>
2025-03-21 15:22:33 -0500

    one backend

--- providers.tf
+++ providers.tf
@@ -13,15 +13,6 @@ terraform {
     }
   }
 
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "rg-infra-state"
-    storage_account_name = "gmezanterraformghactions"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-  }
-
   backend "s3" {
     bucket = "gmezan-terraform-infra-gh-actions"
     key    = "terraform.tfstate"
