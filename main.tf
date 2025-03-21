b4b3eb054d9996cc038ca6cccfdbcc0d89162757
Gustavo Meza <43119289+gmezan@users.noreply.github.com>
2025-03-21 16:13:00 -0500

    Aws/set up (#14)
    * test
    
    * test init
    
    * test fix
    
    * fix?
    
    * one backend
    
    * test plan
    
    * test summary and comment
    
    * test
    
    * deploy
    
    * branch updates
    
    * fix versions
    
    * test
    
    * update docs
    
    * Update README.md
    
    * last change

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-provider "azurerm" {
-  features {}
-  use_oidc = true
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-from-gh"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
