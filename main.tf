8a69dfee79a2f16bf94acf40e8d9db17c75582bf
Gustavo Meza <43119289+gmezan@users.noreply.github.com>
2025-03-21 00:20:53 -0500

    getting started with aks (#9)

--- main.tf
+++ main.tf
@@ -13,20 +13,11 @@ module "rg1" {
 module "acr" {
   source = "./module/azure/acr"
 
-  name     = var.acr_name
-  sku      = var.acr_sku
-  rg_name  = module.rg1.name
-  location = var.location
-
-  depends_on = [module.rg1]
-}
-
-module "aca" {
-  source = "./module/azure/aca"
-
-  name     = "gmezanappcontainerzzz"
-  rg_name  = module.rg1.name
-  location = var.location
+  name          = var.acr_name
+  sku           = var.acr_sku
+  rg_name       = module.rg1.name
+  location      = var.location
+  admin_enabled = true
 
   depends_on = [module.rg1]
 }
