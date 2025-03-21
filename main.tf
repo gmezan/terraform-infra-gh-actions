cfda39b8ec556f3a68cb21a339890774bc81c3c1
gmezan <meza.gustavo@pucp.edu.pe>
2025-03-21 16:26:00 -0500

    clean up resource

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "us-east-2"
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-from-gh"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
