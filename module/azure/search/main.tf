a5f139ee9098a4ae6996ba4d31ea214bfb595703
gmezan <meza.gustavo@pucp.edu.pe>
2025-04-04 10:42:58 -0500

    with both api keys and rbac

--- module/azure/search/main.tf
+++ module/azure/search/main.tf
@@ -4,6 +4,9 @@ resource "azurerm_search_service" "search_service" {
   location            = var.location
   sku                 = var.sku
 
+  local_authentication_enabled = var.local_authentication_enabled
+  authentication_failure_mode  = var.authentication_failure_mode
+
   identity {
     type = var.identity
   }
--- module/azure/search/variables.tf
+++ module/azure/search/variables.tf
@@ -20,3 +20,13 @@ variable "identity" {
   type    = string
   default = "SystemAssigned"
 }
+
+variable "authentication_failure_mode" {
+  type    = string
+  default = "http403"
+}
+
+variable "local_authentication_enabled" {
+  type    = bool
+  default = true
+}
