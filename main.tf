2fb6b6f1edea060b5b9363ee525b461fb06c6c65
gmezan <meza.gustavo@pucp.edu.pe>
2025-04-21 22:21:09 -0500

    with handler

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "./tmp/lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.py"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
